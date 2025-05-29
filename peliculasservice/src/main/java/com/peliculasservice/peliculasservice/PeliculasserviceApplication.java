9267556a168a324093be83c7e729e61c64cb3361
Sebastian Serna <seserna1028@gmail.com>
2025-05-28 19:53:30 -0500

    Inicio Servicio Peliculas

---
+++ peliculasservice/src/main/java/com/peliculasservice/peliculasservice/PeliculasserviceApplication.java
@@ -0,0 +1,13 @@
+package com.peliculasservice.peliculasservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeliculasserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeliculasserviceApplication.class, args);
+	}
+
+}
