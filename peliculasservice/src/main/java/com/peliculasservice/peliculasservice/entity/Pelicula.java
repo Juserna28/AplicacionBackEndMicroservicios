08be6193cf4aba85648a8983e413414ddfae5b0c
Sebastian Serna <seserna1028@gmail.com>
2025-05-30 18:50:50 -0500

    Entidad y Repositorio Base

---
+++ peliculasservice/src/main/java/com/peliculasservice/peliculasservice/entity/Pelicula.java
@@ -0,0 +1,70 @@
+package com.peliculasservice.peliculasservice.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "peliculas")
+public class Pelicula {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String director;
+    private LocalDate releaseDate;
+    private String listedIn;
+    private String description;
+
+    // Getters y Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getListedIn() {
+        return listedIn;
+    }
+
+    public void setListedIn(String listedIn) {
+        this.listedIn = listedIn;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+}
---
+++ peliculasservice/src/main/java/com/peliculasservice/peliculasservice/repository/PeliculaRepository.java
@@ -0,0 +1,14 @@
+package com.peliculasservice.peliculasservice.repository;
+
+import com.peliculasservice.peliculasservice.entity.Pelicula;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+public interface PeliculaRepository extends JpaRepository <Pelicula, Long>  {
+
+    //List <Pelicula> noseaun;
+
+    
+}
