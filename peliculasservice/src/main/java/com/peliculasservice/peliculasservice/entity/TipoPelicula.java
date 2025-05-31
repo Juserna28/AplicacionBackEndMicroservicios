bf2abf87745bd9cec27201898531d6a390a3d95a
Sebastian Serna <seserna1028@gmail.com>
2025-05-31 11:20:11 -0500

    correccion tipo segun abse de datos

--- peliculasservice/src/main/java/com/peliculasservice/peliculasservice/entity/TipoPelicula.java
+++ peliculasservice/src/main/java/com/peliculasservice/peliculasservice/entity/TipoPelicula.java
@@ -1,16 +1,21 @@
 package com.peliculasservice.peliculasservice.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Entity
 @Table(name = "tipo_pelicula")
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TipoPelicula {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id_tipo;
 
-    private String tipo_pelicula;
+    private String tipo;
 }
