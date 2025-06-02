9f19f937365309a2ed54453e10e4477243b61bcc
Sebastian Serna <seserna1028@gmail.com>
2025-06-02 00:32:21 -0500

    e mapeo de la base de datos para 2 tablas nuevas e inciaiacion del nuevo microservicio

---
+++ recomendacionservice/src/main/java/com/recomendacionservice/recomendacionservice/RecomendacionserviceApplication.java
@@ -0,0 +1,13 @@
+package com.recomendacionservice.recomendacionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecomendacionserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecomendacionserviceApplication.class, args);
+	}
+
+}
